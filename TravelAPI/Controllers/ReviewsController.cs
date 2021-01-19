ca1cae1c90687d980f28220896997f2f055d1a5e
ilromali <iliashenkoroman@gmail.com>
2021-01-19 12:04:19 -0800

    Add controllers folder & get index routes for reviews
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>
    Co-authored-by: Joseph Nero <josephnero111@gmail.com>

--- TravelAPI/Controllers/ReviewsController.cs
+++ TravelAPI/Controllers/ReviewsController.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TravelAPI.Models;
+
+namespace TravelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private TravelAPIContext _db;
+        public ReviewsController(TravelAPIContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Review>> Get(string title, int rating, string body, bool wouldRecommend)
+        {
+            return _db.Reviews.ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Review review)
+        {
+            _db.Reviews.Add(review);
+            _db.SaveChanges();
+        }
+
+    }
+}
