04dbdc8bc46391302e0c74c13557178224781fa1
ilromali <iliashenkoroman@gmail.com>
2021-01-20 16:15:45 -0800

    Create new mvc client side folder
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>
    Co-authored-by: Joseph Nero <josephnero111@gmail.com>

---
+++ TravelClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TravelClient
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var apiCallTask = ApiHelper.ApiCall; // ("[YOUR-API-KEY-HERE]"); // Add back into end of line w/ tokenization
+            var result = apiCallTask.Result;
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            Console.WriteLine(jsonResponse["results"]);
+        }
+    }
+    public class ApiHelper
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+        }
+    }
+}
